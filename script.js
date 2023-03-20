ddf713ec8bbbfbdc9dda3df61c2a60dc1da9ea09
gustavos25 <gustavo_souza25@hotmail.com>
2023-03-20 17:15:10 -0300

    0.1

--- script.js
+++ script.js
@@ -4,45 +4,61 @@ var button = document.querySelectorAll(".button-box");
 
 for (var i = 0; i < button.length; i++) {
   var element = button[i];
-  capturarClick(element);
+  captureClick(element);
 }
 //console.log(button[9]);
 visorP.textContent = "";
 visor.appendChild(visorP);
 
-function capturarClick(value) {
+function captureClick(value) {
   value.addEventListener("click", function () {
-    visorP.textContent = value.innerText;
-    console.log("click", value.innerText);
+    if (value.innerText >= 0 || value.innerText <= 9) {
+      console.log(value.innerText);
+    }
+    operationAction(value.innerText);
   });
 }
 
-switch (key) {
-  case "Ac":
-    visorP.textContent = 0;
-    break;
-  case "*":
-    visorP.textContent = 0;
-    break;
-  case "/":
-    visorP.textContent = 0;
-    break;
-  case "X²":
-    visorP.textContent = 0;
-    break;
-  case "√":
-    visorP.textContent = 0;
-    break;
-  case "-":
-    visorP.textContent = 0;
-    break;
-  case "+":
-    visorP.textContent = 0;
-    break;
-  case "=":
-    visorP.textContent = 0;
-    break;
+function result(value) {
+  var word = value.split("");
+  for (var i = 0; i < word.length; i++) {
+    return word[i];
+  }
+}
 
-  default:
-    break;
+function operationAction(operator) {
+  switch (operator) {
+    case "AC":
+      visorP.textContent = 0;
+      break;
+    case "*":
+      visorP.textContent += "*";
+      break;
+    case "/":
+      visorP.textContent += "/";
+      break;
+    case "X²":
+      visorP.textContent += "^";
+      break;
+    case "√":
+      visorP.textContent += "√";
+      break;
+    case "-":
+      visorP.textContent += "-";
+      break;
+    case "+":
+      visorP.textContent += "+";
+      break;
+    case "=":
+      var results = result(visorP.textContent);
+      console.log(results);
+      //if (value.innerText >= 0 || value.innerText <= 9) {
+      //}
+      visorP.textContent = results;
+      break;
+
+    default:
+      visorP.textContent += operator;
+      break;
+  }
 }
