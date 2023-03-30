79a1bcf0622c96321ab95bc4b52fcebc381d3ead
gustavos25 <gustavo_souza25@hotmail.com>
2023-03-30 18:45:44 -0300

    alteracao if para switch e ajustamos raiz quadrada e potencia

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ for (var i = 0; i < button.length; i++) {
   var element = button[i];
   captureClick(element);
 }
-//console.log(button[9]);
+
 visorP.textContent = "";
 visor.appendChild(visorP);
 
@@ -17,26 +17,39 @@ function captureClick(value) {
 }
 
 function showVisor(botao) {
-  if (botao >= 0 || botao <= 9) {
+  if (botao >= 0 || botao <= 9 || botao == ",") {
     visorP.textContent += botao;
-  } else if (botao === "AC") {
-    visorP.textContent = "";
-  } else if (botao === "=") {
-    result();
   } else {
-    visorP.textContent += ` ${botao} `;
+    switch (botao) {
+      case "AC":
+        visorP.textContent = "";
+        break;
+      case "=":
+        result();
+        break;
+      case "x²":
+        visorP.textContent += ` ${botao} `;
+        result();
+        break;
+      case "√":
+        visorP.textContent += ` ${botao} `;
+        result();
+        break;
+
+      default:
+        visorP.textContent += ` ${botao} `;
+    }
   }
 }
 
 function result() {
-  var word = visorP.textContent.split(" ");
+  var replace = visorP.textContent.replaceAll(",", ".");
+  var word = replace.split(" ");
   for (var i = 0; i < word.length; i++) {
-    visorP.textContent = operationAction(
-      Number(word[0]),
-      word[1],
-      Number(word[2])
-    );
+    replace = operationAction(Number(word[0]), word[1], Number(word[2]));
   }
+  replace = replace.toString();
+  visorP.textContent = replace.replaceAll(".", ",");
 }
 
 function operationAction(number1, operator, number2) {
@@ -48,12 +61,11 @@ function operationAction(number1, operator, number2) {
     case "/":
       return (results = number1 / number2);
       break;
-    case "X²":
-      console.log(Math.pow(number1, number2));
-      return (results = Math.pow(number1, number2));
+    case "x²":
+      return (results = Math.pow(number1, 2));
       break;
     case "√":
-      return (results = Math.sqrt(number1, number2));
+      return (results = Math.sqrt(number1, 2));
       break;
     case "-":
       return (results = number1 - number2);
@@ -62,7 +74,7 @@ function operationAction(number1, operator, number2) {
       return (results = number1 + number2);
       break;
     case "=":
-      results;
+      return results;
       break;
 
     default:
